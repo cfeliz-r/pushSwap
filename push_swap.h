c516c8076ab3f869ab7df22f5fb5838c4f0c1dfa
CarlosDavidFelizRamirez <carlos-r40@hotmail.com>
2024-06-06 20:58:43 +0200

    Update libft/ft_printf

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: cfeliz-r <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 14:45:40 by cfeliz-r          #+#    #+#             */
-/*   Updated: 2024/06/06 16:17:57 by cfeliz-r         ###   ########.fr       */
+/*   Updated: 2024/06/06 20:36:50 by cfeliz-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,29 +28,7 @@ typedef struct s_towers
 	char	*log;
 	long	div;
 }					t_towers;
-/*
-int			main(int argc, char **argv);
-char		*joinparams(int agc, char **agv);
-t_towers	organizestruct(int agc, char **agv);
-void		docorrectorder(long *a, long **corr, long size);
-int			checkorder(long *a, long size);
-void		sx(long **list, long size);
-void		px(long **dep, long **arr, long size);
-void		rx(long **list, long size);
-void		rrx(long **list, long size);
-t_towers	chunksort(t_towers tower);
-t_towers	returntoa(t_towers	tower, long *min, long *max);
-void		addlog(char **log, char *a);
-void		printlog(char *log);
-void		improvelog(char **log);
-int			countlog(char *log);
-char		*twonums(long *list);
-char		*threenums(long *list);
-t_towers	bubblesort(t_towers tower);
-t_towers	fivenums(t_towers tower);
-void		slideup(long **list, long size);
-void		slidedwn(long **list, long size);
-void		errmsg(void);*/
+
 int         main(int argc, char **argv);
 char*       join_params(int argc, char **argv);
 t_towers    initialize_struct(int argc, char **argv);
